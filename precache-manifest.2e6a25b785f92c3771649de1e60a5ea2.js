6583c435bc97cf04bc7e3effa4ed18a7c0eac70f
frankdb <fd.barros@yahoo.com>
2019-01-09 17:58:58 -0800

    Updates

---
+++ precache-manifest.2e6a25b785f92c3771649de1e60a5ea2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "225a3d3726835055db4347e8761de9e6",
+    "url": "/weather-app/static/media/snow.225a3d37.jpg"
+  },
+  {
+    "revision": "7a76e065591faaa6448e9b99e155232d",
+    "url": "/weather-app/static/media/rain.7a76e065.jpg"
+  },
+  {
+    "revision": "2bb7dd8e1fa48de6018b085e8c17e79f",
+    "url": "/weather-app/static/media/mist.2bb7dd8e.jpg"
+  },
+  {
+    "revision": "f7653d00150ee7b0f1a7bd1401309f7f",
+    "url": "/weather-app/static/media/clouds.f7653d00.jpg"
+  },
+  {
+    "revision": "9ca3132ac7c52c6710d86a685b313849",
+    "url": "/weather-app/static/media/clear.9ca3132a.jpg"
+  },
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "e48780946644dc514765",
+    "url": "/weather-app/static/js/main.e4878094.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/weather-app/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "e48780946644dc514765",
+    "url": "/weather-app/static/css/main.54318ac7.chunk.css"
+  },
+  {
+    "revision": "c0c9b78079daada7209b7835fe850971",
+    "url": "/weather-app/index.html"
+  }
+];
